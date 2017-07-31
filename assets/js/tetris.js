aaabfc61b34b71c5f33dafd4593572d136040002
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-31 22:08:48 +0100

    DrawMatrix Added

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById('tetris');
 var context = canvas.getContext('2d');
 
+context.scale(20, 20);
+
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -9,9 +11,15 @@ var matrix = [
     [1,1,1],
     [0,1,0],
 ];
-
+function drawMatrix(matrix){
 matrix.forEach((row, y) => {
     row.forEach((value, x) => {
-        
-    })
-})
+        if (value !== 0) {
+            context.fillStyle = 'red';
+            context.fillRect (x, y, 1, 1);
+        }
+    });
+});
+}
+
+drawMatrix(matrix);
