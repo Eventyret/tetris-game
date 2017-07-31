4d2c30f9ad3e0a30998504790288a574ef945b53
Simen Daehlin <simen.dehlin@gmail.com>
2017-07-31 22:25:14 +0100

    Added left right

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -3,14 +3,20 @@ var context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 var matrix = [
     [0,0,0],
     [1,1,1],
     [0,1,0],
 ];
+
+function draw() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawMatrix(player.matrix, player.pos);
+}
 function drawMatrix(matrix, offset){
 matrix.forEach((row, y) => {
     row.forEach((value, x) => {
@@ -24,9 +30,43 @@ matrix.forEach((row, y) => {
     });
 }
 
+var dropCounter = 0;
+var dropInterval = 1000;
+var lastTime = 0;
+
+
+function update(time = 0) {
+    var deltaTime = time - lastTime;
+    lastTime = time;
+
+    
+    // console.log(deltaTime) Getting the time for debug
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
+
 var player = {
     pos:{x: 5, y: 5},
     matrix: matrix,
 }
 
-drawMatrix(player.matrix, player.pos);
+document.addEventListener('keydown', event => {
+    
+    if (event.keyCode === 37) {
+        player.pos.x--; 
+    } else if (event.keyCode == 39){
+        player.pos.x++ ;
+    }
+    
+    // console.log(event); // Getting Keycodes
+    
+})
+
+update();
