b2f944200690bce6aab0b63e6c991bbeae8ad1e4
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-01 20:30:08 +0100

    Added prompt to save playername

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,3 +1,23 @@
+
+
 $( ".btn" ).click(function() {
   $( this ).toggleClass("green");
 });
+
+
+function playerName(z){
+var z = bootbox.prompt("Let's set your playername", function display(z) {
+  if (z != null) {
+      document.getElementById("playername").innerHTML = z;
+      localStorage.playername = z;
+      
+      dis(z);
+  }
+  else
+      document.getElementById("playername").innerHTML = "thankyou";
+})
+};
+function dis(arg) {
+  bootbox.alert("Player name set to " + arg);
+}
+document.getElementById("playername").innerHTML = localStorage.playername;
