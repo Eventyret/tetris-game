a046705b23d3cef25d5757bc766d2848a2b62c34
Simen Daehlin <simen.dehlin@gmail.com>
2017-08-01 07:06:23 +0100

    Added Colours

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -36,37 +36,37 @@ function createPiece(type) {
         ];
     } else if (type === 'O') {
         return [
-            [1,1],
-            [1,1],
+            [2,2],
+            [2,2],
         ];
     } else if (type === 'L') {
         return [
-             [0,1,0],
-             [0,1,0],
-             [0,1,1],
+             [0,3,0],
+             [0,3,0],
+             [0,3,3],
          ];
     } else if (type === 'J') {
         return [
-             [0,1,0],
-             [0,1,0],
-             [1,1,0],
+             [0,4,0],
+             [0,4,0],
+             [4,4,0],
          ];
     } else if (type === 'I') {
         return [
-             [0,1,0,0],
-             [0,1,0,0],
-             [0,1,0,0],
+             [0,5,0,0],
+             [0,5,0,0],
+             [0,5,0,0],
          ];
     } else if (type === 'S') {
         return [
-            [0,1,1],
-            [1,1,0],
+            [0,6,6],
+            [6,6,0],
             [0,0,0],
          ];
     } else if (type === 'Z') {
         return [
-            [1,1,1],
-            [1,0,0],
+            [7,7,7],
+            [7,0,0],
             [0,0,0],
          ];
     }
@@ -84,7 +84,7 @@ function drawMatrix(matrix, offset){
 matrix.forEach((row, y) => {
     row.forEach((value, x) => {
         if (value !== 0) {
-            context.fillStyle = 'red';
+            context.fillStyle = colors[value];
             context.fillRect(x + offset.x,
                              y + offset.y,
                             1, 1);
@@ -186,6 +186,17 @@ function update(time = 0) {
 
     draw();
     requestAnimationFrame(update);
+}
+
+var colors = {
+    null,
+    'red',
+    'blue',
+    'voilet',
+    'green',
+    'purple',
+    'orange',
+    'pink',
 }
 
 var arena = createMatrix(12, 20);
